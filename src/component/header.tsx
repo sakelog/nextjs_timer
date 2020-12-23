bc1d56804814b05d92f5da981e4bb46d2a4a62f6
sake <sakelog.website@gmail.com>
2020-12-23 23:18:54 +0900

    timer修正

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -3,10 +3,14 @@ import Head from 'next/head';
 const Header = () => {
   return (
     <Head>
-      <title>CountDownTimer</title>
+      <title>SimpleCountDownTimer</title>
       <meta
         name="viewport"
         content="minimum-scale=1, initial-scale=1, width=device-width"
+      />
+      <meta
+        name="description"
+        content="Next.jsで作成したカウントダウンタイマー"
       />
       <link
         rel="stylesheet"
