68b99cbf10e5000398b2e06bcc97b41000aafe37
sake <sakelog.website@gmail.com>
2020-12-25 08:06:35 +0900

    構成変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,10 @@
-import { Container, CssBaseline } from '@material-ui/core';
-
-import Header from '../component/header';
-
-import Timer from '../component/timer';
+import Layout from '../component/layout';
 
 const TopPage = () => {
   return (
-    <Container maxWidth="sm">
-      <Header />
-      <CssBaseline />
-      <h1>Simple CountDown Timer</h1>
-      {Timer(180)}
-    </Container>
+    <>
+      <Layout />
+    </>
   );
 };
 
