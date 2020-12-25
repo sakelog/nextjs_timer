ff64bdcb075b907de5c5cf9069f3818e0ac55a9e
sake <sakelog.website@gmail.com>
2020-12-26 08:53:57 +0900

    pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 require('dotenv').config();
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 const withPWA = require('next-pwa');
-
+/*
 module.exports = withPWA({
-  assetPrefix: process.env.GITHUB_PAGES ? '/nextjs_timer' : '',
+  pwa: {
+    dest: 'public',
+  },
+  //phase === PHASE_DEVELOPMENT_SERVER && {basePath: 'test'},
+  //basePath: process.env.GITHUB_PAGES ? '/nextjs_timer' : '',
 });
+*/
+module.exports = (phase, { defaultConfig }) => {
+  withPWA({
+    pwa: {
+      dest: 'public',
+    },
+  });
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      /* development only config options here */
+    };
+  }
+
+  return {
+    /* config options for all phases except development here */
+  };
+};
