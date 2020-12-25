828a706a68c1937cb06e00e954c3f1f3a3996053
sake <sakelog.website@gmail.com>
2020-12-25 23:35:21 +0900

    pwa

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -17,19 +17,6 @@ const Header = () => {
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
       />
       <link rel="manifest" href="/manifest.json" />
-      <link
-        href="/favicon-16x16.png"
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-      />
-      <link
-        href="/favicon-32x32.png"
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-      />
-      <link rel="apple-touch-icon" href="/apple-icon.png"></link>
       <meta name="theme-color" content="#2196f3" />
     </Head>
   );
