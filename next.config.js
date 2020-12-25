d2bd5380bd2f13e1c07e643d64db46624951a532
sake <sakelog.website@gmail.com>
2020-12-25 21:42:22 +0900

    github pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 
 module.exports = {
-  assetPrefix: process.env.GITHUB_PAGES ? '/repository-name' : '',
+  assetPrefix: process.env.GITHUB_PAGES ? '/nextjs_timer' : '',
 };
