050668b52b9e74e87b99125c939fc41119c3e085
sake <sakelog.website@gmail.com>
2020-12-26 11:34:11 +0900

    path変更

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ module.exports = withPWA({
   pwa: {
     dest: 'public',
     scope: process.env.GITHUB_PAGES ? process.env.REPOSITORY_NAME : '/',
+    subdomainPrefix: process.env.GITHUB_PAGES
+      ? process.env.REPOSITORY_NAME
+      : '',
   },
   basePath: process.env.GITHUB_PAGES ? process.env.REPOSITORY_NAME : '',
 });
