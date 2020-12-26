3d21121597a7cfd620bb2379ec2e3324d03b37c5
sake <sakelog.website@gmail.com>
2020-12-26 10:39:13 +0900

    コンフリクト解消

--- next.config.js
+++ next.config.js
@@ -6,3 +6,12 @@ module.exports = withPWA({
   },
   basePath: process.env.GITHUB_PAGES ? process.env.REPOSITORY_NAME : '',
 });
+*/
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+  },
+  basePath: process.env.GITHUB_PAGES
+    ? process.env.GITHUB_REPOSITORY.match(/sakelog\/(.*$)/)
+    : '',
+});
